b868d2a463a18a7243a5d8e64cd8693c30091a11
Christian Kamm <mail@ckamm.de>
2020-03-14 17:26:21 +0100

    Add RALT, NUMS fixes

--- keyboards/ergodox_ez/keymaps/ckamm/keymap.c
+++ keyboards/ergodox_ez/keymaps/ckamm/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,        KC_Y,           KC_U,           KC_I,           KC_O,              KC_P,           KC_NO,
                 KC_H,           KC_J,           KC_K,           LCTL_T(KC_L),      LALT_T(KC_SCLN),KC_RSFT,
   MEH_T(KC_NO), KC_N,           KC_M,           KC_COMM,        KC_DOT,            KC_SLSH,        KC_MINS,
-                                KC_NO,          KC_NO,          KC_LBRC,           KC_RBRC,        KC_NO,
+                                KC_RALT,        KC_NO,          KC_LBRC,           KC_RBRC,        KC_NO,
   KC_NO,   KC_NO,
   KC_PGUP,
   KC_PGDN, KC_TAB, KC_SPC
@@ -72,11 +72,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [ARRO] = LAYOUT_ergodox(
   // left hand
-  KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  _______, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,   KC_NO,
-  KC_NO,   KC_NO,   LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),  KC_NO,   KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_F1,      KC_F2,      KC_F3,      KC_F4,       KC_F5,   KC_NO,
+  KC_NO,   KC_NO,      KC_NO,      KC_NO,      KC_NO,       KC_NO,   KC_NO,
+  _______, KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,       KC_NO,
+  KC_NO,   LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),  KC_NO,   KC_NO,
+  KC_NO,   KC_NO,      KC_NO,      KC_NO,      KC_NO,
                                                KC_NO,   KC_NO,
                                                         KC_NO,
                                    _______,  _______,   KC_NO,
@@ -101,11 +101,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_NO,
                                    _______,  _______,   KC_NO,
   // right hand
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_SLSH, KC_NO,
-  KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    S(KC_8), KC_NO,
-           KC_NO,   KC_4,    KC_5,    KC_6,    KC_MINS, _______,
-  KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    S(KC_EQL),KC_NO,
-                    KC_0,    KC_COMM, KC_DOT,  KC_ENT , KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,
+  KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,     KC_NO,
+           KC_EQL,  KC_4,    KC_5,    KC_6,    S(KC_EQL), S(KC_8),
+  KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    _______,   _______,
+                    KC_0,    KC_COMM, KC_DOT,  KC_ENT ,   KC_NO,
   KC_NO,   KC_NO,
   KC_NO,
   KC_NO,   _______,   _______
@@ -130,6 +130,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,
   KC_NO,   _______,   _______
 ),
+// [RSYM] = LAYOUT_ergodox(
+//   // left hand
+//   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+//   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+//   _______, KC_LALT, KC_NO,   KC_NO,   KC_NO,   KC_NO,
+//   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+//   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+//                                                KC_NO,   KC_NO,
+//                                                         KC_NO,
+//                                    _______,  _______,   KC_NO,
+//   // right hand
+//   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,   KC_NO, KC_NO,
+//   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO,
+//            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO, _______,
+//   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO,
+//                     KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO,
+//   KC_NO,   KC_NO,
+//   KC_NO,
+//   KC_NO,   _______,   _______
+// ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
